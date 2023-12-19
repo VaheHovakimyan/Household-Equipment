c6d0accfb81c76fb2ecdd5efd98254d940a065f5
VaheHovakimyan <88284245+VaheHovakimyan@users.noreply.github.com>
2023-12-19 17:43:28 +0400

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import {FC} from "react";
+import './Home.scss';
+// import {Footer} from "../../components/footer/Footer";
+// import {Header} from "../../components/header/Header";
+// import {WebsiteEnd} from "../../components/websiteEnd/WebsiteEnd";
+
+export const Home: FC = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
